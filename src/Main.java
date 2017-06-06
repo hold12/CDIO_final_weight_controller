239dc894947ad1cb7049714653d8b25185c554f9
Freya Helstrup <freyagh@me.com>
2017-06-06 16:55:06 +0200

    Moved code from Main to new classes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import ctrl.AuthenticateController;
+import ctrl.BatchController;
 import lang.Lang;
 import ctrl.IWeightController;
 import ctrl.WeightController;
@@ -47,7 +49,15 @@ public class Main {
         input = scn.nextLine().toLowerCase();
         if (input.startsWith("batch")) {
             while (true){
-                batch("12", "1234");
+                try {
+                    weightClient.cancelCurrentOperation();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+
+                new AuthenticateController().authenticate(weightClient,"12","1234");
+                new BatchController(weightClient,"12","1234");
+
                 try {
                     while(weightClient.rm208("","Press OK to begin anew.", IWeightController.KeyPadState.NUMERIC).equals("RM20 C"));
                 } catch (IOException e) {
@@ -60,144 +70,6 @@ public class Main {
         }
 
         menu();
-    }
-
-    private void batch(String userId, String batchId) {
-        try {
-            weightClient.cancelCurrentOperation();
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-        String userInput = "";
-
-        if(!authenticate(userId, batchId)) return;
-        //System.out.println("User authenticated");
-
-        // Verify Username
-        try {
-            userInput = weightClient.rm208("VERIFY", "andersand", IWeightController.KeyPadState.UPPER_CHARS);
-        } catch (IOException e) { System.err.println(Lang.msg("exceptionRM208")); }
-
-        if (!userInput.equals("Y")) {
-            batch(userId, batchId); return;
-        }
-
-        // Verify Batch
-        try {
-            userInput = weightClient.rm208("VERIFY", "salt", IWeightController.KeyPadState.UPPER_CHARS);
-        } catch (IOException e) { System.err.println(Lang.msg("exceptionRM208")); }
-
-        if (!userInput.equals("Y")) {
-            batch(userId, batchId); return;
-        }
-        //System.out.println("User verified information.");
-
-        // Unloaded
-        try {
-            userInput = weightClient.rm208("VERIFY", "unloaded", IWeightController.KeyPadState.UPPER_CHARS);
-        } catch (IOException e) { System.err.println(Lang.msg("exceptionRM208")); }
-
-        if (!userInput.equals("Y")) {
-            batch(userId, batchId); return;
-        }
-        //System.out.println("Weight is unloaded.");
-
-        // Place tara
-        try {
-            userInput = weightClient.rm208("", "Place tare", IWeightController.KeyPadState.UPPER_CHARS);
-        } catch (IOException e) { System.err.println(Lang.msg("exceptionRM208")); }
-
-        //System.out.println("Tare placed.");
-        float tareWeight = 0;
-        try {
-            tareWeight = stof(weightClient.tareWeight());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //System.out.println("Weight tared as " + tareWeight + " kg.");
-
-        try {
-            userInput = weightClient.rm208("", "Place powder", IWeightController.KeyPadState.UPPER_CHARS);
-        } catch (IOException e) { System.err.println(Lang.msg("exceptionRM208")); }
-
-        float netWeight = 0;
-        try {
-            netWeight = stof(weightClient.getCurrentWeight());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //System.out.println("Current weight is " + netWeight + " kg.");
-
-        try {
-            userInput = weightClient.rm208("", "Remove all", IWeightController.KeyPadState.UPPER_CHARS);
-        } catch (IOException e) { System.err.println(Lang.msg("exceptionRM208")); }
-
-        float removedWeight = 0;
-        try {
-            removedWeight = stof(weightClient.getCurrentWeight());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        /*System.out.println(removedWeight);
-        System.out.println(((-tareWeight)*1.05));
-        System.out.println(((-tareWeight)*0.95));*/
-        if (removedWeight >= ((-tareWeight)*1.05) && removedWeight <= ((-tareWeight)*0.95)) {
-            try {
-                weightClient.writeToPrimaryDisplay("OK");
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            } catch (IOException e) {
-                System.err.print(Lang.msg("exceptionMessageDelivery"));
-            }
-        }
-        else {
-            try {
-                weightClient.writeToPrimaryDisplay("TareErr");
-                weightClient.writeToSecondaryDisplay("Taring not approved. Try again");
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            } catch (IOException e) {
-                System.err.print(Lang.msg("exceptionMessageDelivery"));
-            }
-        }
-    }
-
-    private boolean authenticate(String userId, String batchId) {
-        String userIdIn = "";
-        String batchIdIn = "";
-        try {
-            userIdIn = weightClient.rm208("User", "Type user id", IWeightController.KeyPadState.NUMERIC);
-        } catch (IOException e) {
-            System.err.println(Lang.msg("exceptionRM208"));
-        }
-
-        if (!userIdIn.equals(userId)) {
-            System.err.println(Lang.msg("errNotAuthenticated"));
-            try { weightClient.writeToPrimaryDisplay("500"); weightClient.writeToSecondaryDisplay(Lang.msg("errNotAuthenticated")); }
-            catch (IOException e) { System.err.println(e.getMessage()); }
-            return false;
-        }
-
-        try {
-            batchIdIn = weightClient.rm208("Batch", "Type batch id", IWeightController.KeyPadState.NUMERIC);
-        } catch (IOException e) {
-            System.err.println(Lang.msg("exceptionRM208"));
-        }
-
-        if (!batchIdIn.equals(batchId)) {
-            System.err.println(Lang.msg("errNoBatch"));
-            try { weightClient.writeToPrimaryDisplay("404"); weightClient.writeToSecondaryDisplay(Lang.msg("errNoBatch")); }
-            catch (IOException e) { System.err.println(e.getMessage()); }
-            return false;
-        }
-        return true;
     }
 
     private boolean connect() {
@@ -231,16 +103,6 @@ public class Main {
             return Integer.parseInt(str);
         } catch (Exception e) {
             System.err.println(Lang.msg("errSTOI") + "!");
-            return -1;
-        }
-    }
-
-    private static float stof(String str) {
-        try {
-            str = str.replace(",",".");
-            return Float.parseFloat(str);
-        } catch (Exception e) {
-            System.err.println(Lang.msg("errSTOF") + "!");
             return -1;
         }
     }
