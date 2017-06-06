a6fc09d95633275df80f04a59fedd528c198590f
Freya Helstrup <freyagh@me.com>
2017-06-06 16:41:39 +0200

    New JDBC library and package names

--- src/dao/IUserDAO.java
+++ src/dao/IUserDAO.java
@@ -1,11 +1,9 @@
 package dao;
 
-import java.util.List;
 import jdbclib.DALException;
 
 import dto.UserDTO;
 
 public interface IUserDAO {
 	UserDTO getUser(int userId) throws DALException;
-	List<UserDTO> getUserList() throws DALException;
 }
